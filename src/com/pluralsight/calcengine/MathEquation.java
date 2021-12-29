3e9d639099c18e0a80c5bf563998f213559f6cc7
Richard Yvarra <richard.yvarra@ibm.com>
2021-12-29 13:39:33 -0500

    first commit

--- src/com/pluralsight/calcengine/MathEquation.java
+++ src/com/pluralsight/calcengine/MathEquation.java
@@ -3,7 +3,7 @@ package com.pluralsight.calcengine;
 public class MathEquation {
     private double leftVal;
     private double rightVal;
-    private char opCode;
+    private char opCode ='a';
     private double result;
 
     public double getLeftVal() {return leftVal;}
@@ -15,9 +15,33 @@ public class MathEquation {
 
     public double getResult() {return result;}
 
+    public MathEquation(){}
 
 
 
+    public MathEquation(char opCode){
+        this.opCode = opCode;
+    }
+    public MathEquation(char opCode, double leftVal, double rightVal){
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
+    public void execute(double leftVal, double rightVal){
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+        execute();
+
+    }
+    public void execute(int leftVal, int rightVal){
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+        execute();
+        result = (int)result;
+
+    }
     public void execute(){
         switch(opCode) {
             case 'a':
